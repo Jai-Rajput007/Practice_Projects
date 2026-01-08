14b69b13e3aae058a0b355bdf8effe01f1410f2a
Jai-Rajput007 <136909371+Jai-Rajput007@users.noreply.github.com>
2026-01-08 09:34:10 +0530

    Update

--- 100 days of code/Day_2/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-print("Welcome to the tip calculator !!")
-Bill = float(input("What was the total bill ? $"))
-Tip = int(input("How much tip would you like to give ? 10, 12, or 15?"+" "))
-People = int(input("How many people to split the bill? "))
-ans = (Bill + Tip)/People
-print(f"Each person should pay : ${ans}")
